e75c67ae6ec1432ce3f3f75efd14f7c057829d8f
KitKat <codedoes@gmail.com>
2017-01-29 23:02:02 +0200

    added property & method argument type

--- modules/dlscript/api_generator.cpp
+++ modules/dlscript/api_generator.cpp
@@ -3,7 +3,7 @@
 
 #include "class_db.h"
 #include "os/file_access.h"
-#include "path_utils.h"
+//#include "path_utils.h"
 #include "core/globals.h"
 
 
@@ -36,23 +36,36 @@ static Error save_file(const String& p_path, const List<String>& p_content) {
 struct MethodAPI {
 	String method_name;
 	String return_type;
-
+	
 	List<String> argument_types;
 	List<String> argument_names;
-
+	
 	/*
-	struct CNames {
-		String method_name;
-		String return_type;
+	 *	struct CNames {
+	 *		String method_name;
+	 *		String return_type;
+	 * 
+	 *		List<String> argument_types;
+	 *		List<String> argument_names;
+	 * 
+	 *		List<String> special_return_arg_names;
+	 *		List<String> special_return_arg_types;
+	 * 
+} c;
+*/
+};
 
-		List<String> argument_types;
-		List<String> argument_names;
 
-		List<String> special_return_arg_names;
-		List<String> special_return_arg_types;
+struct PropertyAPI {
+	String prop_name;
+	String prop_getter;
+	String prop_setter;
+	String prop_type;
+};
 
-	} c;
-	*/
+struct ConstantAPI {
+	String constant_name;
+	String constant_value;
 };
 
 struct ClassAPI {
@@ -65,6 +78,8 @@ struct ClassAPI {
 	bool memory_own;
 
 	List<MethodAPI> methods;
+	List<PropertyAPI> properties;
+	List<ConstantAPI> constants;
 
 	// constants
 };
@@ -87,7 +102,7 @@ List<ClassAPI> generate_c_api_classes() {
 
 
 	for (List<StringName>::Element *e = classes.front(); e != NULL; e = e->next()) {
-
+		
 		ClassDB::APIType api_type = ClassDB::get_api_type(e->get());
 
 		StringName class_name = e->get();
@@ -103,8 +118,31 @@ List<ClassAPI> generate_c_api_classes() {
 			is_reference = inheriters.find(class_name) < 0;
 			class_api.memory_own = !class_api.is_singleton && is_reference;
 		}
-
-
+		//TODO constants name value
+		//constants
+		/*List<String> constant;
+		ClassDB::get_integer_constant_list(class_name, &constant);
+		for (List<String>::Element *c = constants.front(); c != NULL; c = c->next()){
+			ConstantAPI constant_api;
+			ClassDB::get_integer_constant;
+			
+		}*/
+		
+		//properties
+		List<PropertyInfo> properties;
+		ClassDB::get_property_list(class_name, &properties, true);
+		
+		for (List<PropertyInfo>::Element *p = properties.front(); p != NULL; p = p->next()) {
+			PropertyAPI property_api;
+			
+			property_api.prop_getter=ClassDB::get_property_getter(class_name,p->get().name);
+			property_api.prop_setter=ClassDB::get_property_setter(class_name,p->get().name);
+			property_api.prop_type=Variant::get_type_name(ClassDB::get_property_type(class_name,p->get().name));
+			
+			class_api.properties.push_back(property_api);
+		}
+		
+		//methods
 		List<MethodInfo> methods;
 		ClassDB::get_method_list(class_name, &methods, true);
 
@@ -113,8 +151,9 @@ List<ClassAPI> generate_c_api_classes() {
 			MethodAPI method_api;
 			MethodBind *method_bind = ClassDB::get_method(class_name, m->get().name);
 
+			//method name
 			method_api.method_name = m->get().name;
-
+			//method return type
 			if (method_bind && method_bind->get_return_type() != StringName()) {
 				method_api.return_type = method_bind->get_return_type();
 			} else if (method_api.method_name.find(":") != -1) {
@@ -128,11 +167,11 @@ List<ClassAPI> generate_c_api_classes() {
 				method_api.return_type = "void";
 			}
 
-
+			// method argument name and type
 			List<PropertyInfo> arguments = m->get().arguments;
 			for (List<PropertyInfo>::Element *a = arguments.front(); a != NULL; a = a->next()) {
 				method_api.argument_names.push_back(a->get().name);
-				// TODO: argument types
+				method_api.argument_types.push_back(Variant::get_type_name(a->get().type));
 			}
 
 			class_api.methods.push_back(method_api);
