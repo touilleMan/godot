c1c24b3d97df8addceb9f262cd5a33b37d2fa339
Karroffel <therzog@mail.de>
2017-02-28 00:57:40 +0100

    implemented color api

--- modules/dlscript/core_api/godot_color.h
+++ modules/dlscript/core_api/godot_color.h
@@ -9,7 +9,11 @@ extern "C" {
 #include "../godot_core_api.h"
 
 void GDAPI godot_color_new(godot_color *p_color);
-void GDAPI godot_color_destroy(godot_color *p_color);
+void GDAPI godot_color_new_rgba(godot_color *p_color, const godot_real r, const godot_real g, const godot_real b, const godot_real a);
+
+uint32_t GDAPI godot_color_get_32(const godot_color *p_color);
+
+float GDAPI *godot_color_index(godot_color *p_color, const godot_int idx);
 
 
 #ifdef __cplusplus
